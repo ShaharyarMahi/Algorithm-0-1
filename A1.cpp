73d3408e0ac5bc08fe4fcdc3f4edcda40fd578f3
Mahir Shaharyar Zzaman <158563822+ShaharyarMahi@users.noreply.github.com>
2024-06-23 15:49:07 +0600

    Add files via upload

---
+++ A1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,b; cin>>a>>b;
+	cout<<"entered two number are : "<<a<<" "<<b<<endl;
+	swap(a,b);
+	cout<<"After Swap : "<<a<<" "<<b<<endl;
+
+	return 0;
+}
